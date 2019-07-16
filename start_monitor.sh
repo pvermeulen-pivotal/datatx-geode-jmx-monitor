64b7b26bfc0aa68e987245086833c137b450651f
Paul Vermeulen <pvermeulen@pivotal.io>
2019-07-16 13:01:05 -0500

    Update start_monitor.sh

--- start_monitor.sh
+++ start_monitor.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-$JAVA_HOME/bin/java -cp /usr/bin/monitor/lib/* util.geode.monitor.jmx.StartMonitor
+java -cp /usr/bin/monitor/conf:/usr/bin/monitor/lib/* util.geode.monitor.jmx.StartMonitor
+
